a22ad9a472d6fc63aca139d78537f1947e0aaea6
antonio-datahack <antoniosastre@tutanota.com>
2022-07-05 10:13:47 +0200

    deleted white spaces

--- routes/auth.js
+++ routes/auth.js
@@ -1,53 +1,50 @@
 const router = require("express").Router();
-const User = require('../models/User')
-const bcrypt = require('bcryptjs');
+const User = require("../models/User");
+const bcrypt = require("bcryptjs");
 const { isLoggedOut } = require("../middleware/route-guard");
 const saltRounds = 10;
 
-router.get('/signup', isLoggedOut, (req, res, next) => {
-    res.render('signup')
-})
+router.get("/signup", isLoggedOut, (req, res, next) => {
+  res.render("signup");
+});
 
-router.post('/signup', isLoggedOut, (req, res, next) => {
-    const {username, email, /* phoneNumber, */ password} = req.body;
-    if(password.length < 4) {
-        res.render('signup', {message: 'Password has to be minimum 4 characters.'})
-        return;
-    };
-    if (username === '') {
-        res.render('signup', {message: 'Username cannot be empty.'})
-    };
-    if (password.length === ''){
-        res.render('signup', {message: 'Password cannot be empty'})
-    }
-    User.findOne({ username: username }).then((userFromDB) => {
-    if (userFromDB !== null) {
-        res.render("signup", { message: "The username is already taken" });
-        return;
-
-    } else {
-        const salt = bcrypt.genSaltSync();
-        const hash = bcrypt.hashSync(password, salt);
-
-        User.create({
-          username: username,
-          password: hash,
-          email: email,
-        })
-          .then((createdUser) => {
-            console.log(createdUser);
-            res.redirect("/login");
-          })
-          .catch((err) => {
-            next(err);
-          })
-      }
+router.post("/signup", isLoggedOut, (req, res, next) => {
+  const { username, email, /* phoneNumber, */ password } = req.body;
+  if (password.length < 4) {
+    res.render("signup", {
+      message: "Password has to be minimum 4 characters.",
     });
-  });
-<<<<<<< HEAD
-=======
+    return;
+  }
+  if (username === "") {
+    res.render("signup", { message: "Username cannot be empty." });
+  }
+  if (password.length === "") {
+    res.render("signup", { message: "Password cannot be empty" });
+  }
+  User.findOne({ username: username }).then((userFromDB) => {
+    if (userFromDB !== null) {
+      res.render("signup", { message: "The username is already taken" });
+      return;
+    } else {
+      const salt = bcrypt.genSaltSync();
+      const hash = bcrypt.hashSync(password, salt);
 
->>>>>>> master
+      User.create({
+        username: username,
+        password: hash,
+        email: email,
+      })
+        .then((createdUser) => {
+          console.log(createdUser);
+          res.redirect("/login");
+        })
+        .catch((err) => {
+          next(err);
+        });
+    }
+  });
+});
 
 router.get("/login", (req, res, next) => {
   res.render("login");
