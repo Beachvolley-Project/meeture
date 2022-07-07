3c4d6bb4ef938c714284aef01140a041d300c184
antonio-datahack <antoniosastre@tutanota.com>
2022-07-07 21:52:54 +0200

    add bootstrap to event-list edit-list

--- models/User.js
+++ models/User.js
@@ -19,18 +19,6 @@ const userSchema = new Schema(
       enum: ["user", "admin"],
       default: "user",
     },
-    email: {
-      type: String,
-      trim: true,
-      lowercase: true,
-      unique: true,
-      //required: [true, "Email address is required"],
-      /* validate: [validateEmail, "Please fill a valid email address"],
-      match: [
-        /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
-        "Please fill a valid email address",
-      ] */
-    },
   },
   {
     timestamps: true,
